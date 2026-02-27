3ac74f2a14a275516b3768774a70f95cf010e4a7
Lautaro Poma <lautaropoma@MBPdeGuillermo.fibertel.com.ar>
2026-02-27 09:05:27 -0300

    Implement SystemPerformanceManager, BaseSystemUtilTask, SystemCpuUtilTask and ConstrainedDeviceApp

--- src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
+++ src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
@@ -13,6 +13,7 @@
 import logging
 
 from time import sleep
+from programmingtheiot.cda.system.SystemPerformanceManager import SystemPerformanceManager
 
 logging.basicConfig(format = '%(asctime)s:%(name)s:%(levelname)s:%(message)s', level = logging.DEBUG)
 
@@ -29,8 +30,8 @@ class ConstrainedDeviceApp():
 		@param path The name of the resource to apply to the URI.
 		"""
 		logging.info("Initializing CDA...")
-		
-		# TODO: implementation here
+
+		self.sysPerfManager = SystemPerformanceManager()
 
 	def startApp(self):
 		"""
@@ -39,7 +40,7 @@ class ConstrainedDeviceApp():
 		"""
 		logging.info("Starting CDA...")
 		
-		# TODO: implementation here
+		self.sysPerfManager.startManager()
 		
 		logging.info("CDA started.")
 
@@ -50,7 +51,7 @@ class ConstrainedDeviceApp():
 		"""
 		logging.info("CDA stopping...")
 		
-		# TODO: implementation here
+		self.sysPerfManager.stopManager()
 		
 		logging.info("CDA stopped with exit code %s.", str(code))
 		
